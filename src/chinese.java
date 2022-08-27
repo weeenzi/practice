80c1a14908772c1f2af67c7fbb76a1d562200cb0
wenzi <651579145@qq.com>
2022-08-27 21:29:42 +0800

    8.27,sec

--- src/chinese.java
+++ src/chinese.java
@@ -5,5 +5,9 @@ public class chinese {
         zhangsan.name="章三";
         System.out.println(zhangsan.name+"的id是"+zhangsan.id+"他的国际是"+zhangsan.guoji);
 
+        DemoStatic lisi =new DemoStatic();
+        lisi.id=2;
+        lisi.name="李四";
+        System.out.println(lisi.name+"的id是"+lisi.id+"他的国籍是"+lisi.guoji);
     }
 }
