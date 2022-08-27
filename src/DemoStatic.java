e96e194efd777401a0f69b911d03636afc3ba80e
wenzi <651579145@qq.com>
2022-08-27 21:23:05 +0800

    8.27

---
+++ src/DemoStatic.java
@@ -0,0 +1,14 @@
+public class DemoStatic {
+    static{
+        System.out.println("method看好了 static是这么用的!");
+    }
+    int id;
+    String name;
+    static String guoji="中国";
+
+    public  void DemoStatic(int id,String name){
+        this.id = id;
+        this.name = name;
+
+    }
+}
---
+++ src/chinese.java
@@ -0,0 +1,9 @@
+public class chinese {
+    public static void main(String[] args) {
+        DemoStatic zhangsan =new DemoStatic();
+        zhangsan.id=1;
+        zhangsan.name="章三";
+        System.out.println(zhangsan.name+"的id是"+zhangsan.id+"他的国际是"+zhangsan.guoji);
+
+    }
+}
