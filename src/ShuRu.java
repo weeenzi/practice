3fa3d433b38690adb36c140f36d64eb773d32ae0
wenzi <651579145@qq.com>
2022-08-28 13:28:50 +0800

    8.28 first

---
+++ src/ShuRu.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ShuRu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("输入数字已算出他们的平均值");
+
+        double sum = 0;
+        double mix = 0;
+        while(sc.hasNextDouble()){
+            System.out.println("请输入数字或者end结束");
+          double   a = sc.nextDouble();
+            sum += 1;
+            mix += a;
+        }
+        System.out.println("这些数字的平均数是" + mix/sum);
+
+
+    }
+}
